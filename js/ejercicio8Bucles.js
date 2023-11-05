57c6cc0b9a0e2ae3c38064ab95eb17e19389a0cd
JulianAballay <juliancabj1210@gmail.com>
2023-11-05 01:23:31 -0300

    Agregue logica ejercicio 8 Bucles

--- js/ejercicio8Bucles.js
+++ js/ejercicio8Bucles.js
@@ -2,10 +2,9 @@ let valorPiramide = parseInt(
   prompt("Ingrese un valor para armar la piramide: ")
 );
 if (valorPiramide >= 1 && valorPiramide <= 50) {
-  for (let i = 1; i <= valorPiramide; i++) {
-    for (let j = 0; j < i; j++) {
-        
-        document.write('1' + i);
+  for (let i = 0; i < valorPiramide; i++) {
+    for (let j = 1; j <= i; j++) {
+        document.write(j);
     }
     document.write("<br>");
   }
