da5ea3072c9128b972b392a31936dbd8798b3f1c
JulianAballay <juliancabj1210@gmail.com>
2023-11-06 01:41:21 -0300

    Termine de mejorar logica modificando bucles - ejercicio 8

--- js/ejercicio8Bucles.js
+++ js/ejercicio8Bucles.js
@@ -2,7 +2,7 @@ let valorPiramide = parseInt(
   prompt("Ingrese un valor para armar la piramide: ")
 );
 if (valorPiramide >= 1 && valorPiramide <= 50) {
-  for (let i = 0; i < valorPiramide; i++) {
+  for (let i = 1; i <= valorPiramide; i++) {
     for (let j = 1; j <= i; j++) {
         document.write(j);
     }
