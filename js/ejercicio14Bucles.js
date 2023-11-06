ae29658ba6a76641e12b8f279de4cb72252242b1
JulianAballay <juliancabj1210@gmail.com>
2023-11-05 23:23:27 -0300

    a

---
+++ js/ejercicio14Bucles.js
@@ -0,0 +1,8 @@
+let cadena = prompt("Ingrese el texto: ");
+let cadenaFormateada = " ";
+for (let i = 0; i < cadena.length; i++) {
+  if ("cadena"[i] != " ") {
+    cadena.push("cadena"[i] + "-");
+  }
+}
+console.log(cadena);
