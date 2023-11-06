ec5013cf48f467cb9e995ab3993f3f2957ed42de
JulianAballay <juliancabj1210@gmail.com>
2023-11-06 01:28:20 -0300

    Mejoro logica ejercicio 2

--- js/ejercicio2Bucles.js
+++ js/ejercicio2Bucles.js
@@ -1,23 +1,31 @@
-const nota = parseInt(prompt("Ingrese la nota para conocer su calificación: "));
-if(nota >= 0 && nota <= 10){
-    if(nota >= 0 && nota <=2){
-        alert("Muy deficiente");
+do{
+    let nota = parseInt(prompt("Ingrese la nota para conocer su calificación: "));
+    if(Number.isInteger(nota)){
+        if(nota >= 0 && nota <= 10){
+            if(nota >= 0 && nota <=2){
+                alert("Muy deficiente");
+            }
+            if(nota >= 3 && nota <= 4){
+                alert("Insuficiente");
+            }
+            if(nota >= 5 && nota <= 6){
+                alert("Suficiente");
+            }
+            if(nota == 7){
+                alert("Bien");
+            }
+            if(nota >= 8 && nota <= 9){
+                alert("Notable");
+            }
+            if(nota == 10){
+                alert("Sobresaliente");
+            }
+        }else{
+            alert("número erroneo!!");
+        }
+    }else{
+        alert("Introduce un número válido")
     }
-    if(nota >= 3 && nota <= 4){
-        alert("Insuficiente");
-    }
-    if(nota >= 5 && nota <= 6){
-        alert("Suficiente");
-    }
-    if(nota == 7){
-        alert("Bien");
-    }
-    if(nota >= 8 && nota <= 9){
-        alert("Notable");
-    }
-    if(nota == 10){
-        alert("Sobresaliente");
-    }
-}else{
-    alert("Valor ingresado no valido como nota");
-}
+    
+}while(confirm("Desea seguir ingresando notas? "));
+
