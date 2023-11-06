1b35397eda85739bbc5d1285b65751ff3b046cdc
JulianAballay <juliancabj1210@gmail.com>
2023-11-06 01:21:43 -0300

    Mejoro logica ejercicio 1

--- js/ejercicio1Bucles.js
+++ js/ejercicio1Bucles.js
@@ -1,6 +1,10 @@
 const edad = parseInt(prompt("Ingrese su edad: "));
-if(edad >= 18){
-    document.write('<h2 class="text-center text-success">Tiene permiso para conducir!!!</h2>')
+if(Number.isInteger(edad)){
+    if(edad >= 18){
+        document.write('<h2 class="text-center text-success">Tiene permiso para conducir!!!</h2>')
+    }else{
+        document.write('<h2 class="text-center text-danger">Usted no tiene permiso para conducir</h2>')
+    }
 }else{
-    document.write('<h2 class="text-center text-danger">Usted no tiene permiso para conducir</h2>')
+    document.write('<h2 class="text-center text-danger">El valor ingresado no es una edad</h2>');
 }
