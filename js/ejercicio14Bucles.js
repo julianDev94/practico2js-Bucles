2aab7281da909e230d3dd4bc6e3adc67a79c92d4
JulianAballay <juliancabj1210@gmail.com>
2023-11-05 23:13:49 -0300

    modifico codigo de ejercicio 14 Bucles - sin solucion aun

---
+++ js/ejercicio14Bucles.js
@@ -0,0 +1,9 @@
+let cadena = prompt("Ingrese el texto: ");
+let cadenaFormateada = ' ';
+for(let i = 0; i < cadena.length ; i++){
+    if("cadena"[i] != ' '){
+        "cadena"[i] = "cadena"[i] + '-';
+    }
+}
+console.log(cadena);
+
